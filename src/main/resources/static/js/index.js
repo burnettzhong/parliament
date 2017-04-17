a92bda9d812d060b6acf4ed3c4b3412982a8f1f3
Zhong Han <burnetzhong@yeah.net>
2017-04-17 18:26:00 +0800

    Refine vue.js code

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,27 +1,24 @@
-$(function () {
-    $.ajax({
-        url: 'api/',
-        datatype: 'json',
-        type: "get",
-        contentType: "application/json",
-        success: function (data) {
-            apiresource = data;
-            $.each(data, function(i, v){
-                app4.apis.push(v);
-
-            });
-        },
-        error: function () {
-            alert("An error during data retrieving. Please, try again later");
-        }
-    });
-});
-
-// Vue
-var apiresource = {};
-var app4 = new Vue({
-    el: '#app-4',
+var apiList = new Vue({
+    el: '#api-list',
     data: {
         apis: []
+    },
+    mounted: function () {
+        console.log("done");
+        $.ajax({
+            url: 'api/',
+            datatype: 'json',
+            type: "get",
+            contentType: "application/json",
+            success: function (data) {
+                $.each(data, function(i, v){
+                    apiList.apis.push(v);
+
+                });
+            },
+            error: function () {
+                alert("An error during data retrieving. Please, try again later");
+            }
+        });
     }
 });
